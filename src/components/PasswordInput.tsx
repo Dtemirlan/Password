62a2864ad4c2ae2b820f7370b048e6ce9a067ec7
Dzhaparov Temirlan <temirlandzaparov820@gmail.com>
2023-12-14 13:30:19 +0600

    Настроена функция handleButtonClick для удаления последнего символа из введенного пароля

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setInput, clearInput, setCorrect, setMessage, selectPassword } from '../features/passwordSlice';
+import { setInput, setCorrect, setMessage, selectPassword } from '../features/passwordSlice';
 import './PasswordInput.css';
 
 const PasswordInput: React.FC = () => {
@@ -9,14 +9,16 @@ const PasswordInput: React.FC = () => {
 
     const handleButtonClick = (value: string) => {
         if (value === '<') {
-            dispatch(clearInput());
+            if (input.length > 0) {
+                dispatch(setInput(input.slice(0, -1)));
+            }
         } else if (value === 'E') {
             if (input === '1337') {
                 dispatch(setCorrect(true));
-                dispatch(setMessage('Access Granted'));
+                dispatch(setMessage('Доступ разрешен'));
             } else {
                 dispatch(setCorrect(false));
-                dispatch(setMessage('Access Denied'));
+                dispatch(setMessage('Доступ запрещен'));
             }
         } else if (input.length < 4) {
             dispatch(setInput(input + value));
@@ -29,22 +31,25 @@ const PasswordInput: React.FC = () => {
         <div className="password-container">
             <div className="input-container">
                 <div className="digits-container">
+                    {/* Отображаем цифры введенного пароля */}
                     {input.split('').map((digit, index) => (
                         <span key={index} className={digitClass}>
               {digit}
             </span>
                     ))}
                 </div>
-                <div className="invalid-feedback">Incorrect input</div>
-                <div className="valid-feedback">Correct input</div>
+                <div className="invalid-feedback">Неправильный ввод</div>
+                <div className="valid-feedback">Верный ввод</div>
             </div>
             <div className="button-container">
+                {/* Отображаем кнопки для ввода цифр и управления паролем */}
                 {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '<', 'E'].map((button) => (
                     <button key={button} className="btn btn-secondary button" onClick={() => handleButtonClick(button)}>
                         {button}
                     </button>
                 ))}
             </div>
+            {/* Отображаем сообщение о результате ввода пароля */}
             <div className={`message ${isCorrect ? 'text-success' : 'text-danger'}`}>{message}</div>
         </div>
     );
